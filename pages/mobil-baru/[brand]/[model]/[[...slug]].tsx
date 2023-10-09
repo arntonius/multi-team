b90626b883d0fe21c6f95d8e34cfa8cfeb1a48ee
renaldi-tri <renaldi.tri@dot-indonesia.com>
2023-10-09 15:25:27 +0700

    fix contenturl if images doesnt match

--- pages/mobil-baru/[brand]/[model]/[[...slug]].tsx
+++ pages/mobil-baru/[brand]/[model]/[[...slug]].tsx
@@ -655,7 +655,8 @@ const jsonLD = (
     ImageObject: [
       {
         '@type': 'ImageObject',
-        contentUrl: filterImageBasedOnType(carModel?.images, 'eksterior')?.[0],
+        contentUrl: (filterImageBasedOnType(carModel?.images, 'eksterior') as string[]).length ?
+          filterImageBasedOnType(carModel?.images, 'eksterior')?.[0] : carModel?.images?.[0],
         mainEntityOfPage: `https://www.seva.id/mobil-baru/${carModel?.brand}/${carModel?.model}?tab=Eksterior`,
         representativeOfPage: 'https://schema.org/True',
         isFamilyFriendly: 'https://schema.org/True',
@@ -663,7 +664,8 @@ const jsonLD = (
       },
       {
         '@type': 'ImageObject',
-        contentUrl: filterImageBasedOnType(carModel?.images, 'interior')?.[0],
+        contentUrl: (filterImageBasedOnType(carModel?.images, 'eksterior') as string[]).length ?
+          filterImageBasedOnType(carModel?.images, 'interior')?.[0] : carModel?.images?.[0],
         mainEntityOfPage: `https://www.seva.id/mobil-baru/${carModel?.brand}/${carModel?.model}?tab=Interior`,
         representativeOfPage: 'https://schema.org/True',
         isFamilyFriendly: 'https://schema.org/True',
