34fa5c4a6d589131924f93ca400477a321a0afa1
toni612 <antonius.ifabula@ifabula.com>
2023-10-16 10:22:55 +0700

    SEVA-5713 LC UI enchancement P0

--- pages/kalkulator-kredit/[[...slug]].tsx
+++ pages/kalkulator-kredit/[[...slug]].tsx
@@ -1726,6 +1726,7 @@ export default function LoanCalculatorPage() {
                 name="city"
                 onOpenTooltip={onOpenTooltipCityField}
                 onShowDropdown={onShowDropdownCityField}
+                isError={isValidatingEmptyField && !forms.city}
               />
               {isValidatingEmptyField && !forms.city
                 ? renderErrorMessageEmpty()
@@ -1745,6 +1746,10 @@ export default function LoanCalculatorPage() {
                 allModelCarList={allModelCarList}
                 setModelError={setModelError}
                 onShowDropdown={onShowDropdownModelField}
+                overrideIsErrorFieldOnly={
+                  isValidatingEmptyField &&
+                  (!forms.model?.modelId || !forms.model.modelName)
+                }
               />
               {isValidatingEmptyField &&
               (!forms.model?.modelId || !forms.model.modelName)
@@ -1760,6 +1765,10 @@ export default function LoanCalculatorPage() {
                 value={forms.variant || variantEmptyValue}
                 modelError={modelError}
                 onShowDropdown={onShowDropdownVariantField}
+                isError={
+                  isValidatingEmptyField &&
+                  (!forms.variant?.variantId || !forms.variant.variantName)
+                }
               />
               {isValidatingEmptyField &&
               (!forms.variant?.variantId || !forms.variant.variantName)
@@ -1773,7 +1782,10 @@ export default function LoanCalculatorPage() {
                 value={Number(forms.monthlyIncome)}
                 defaultValue={Number(forms.monthlyIncome)}
                 handleChange={handleChange}
-                isErrorTooLow={isIncomeTooLow}
+                isError={
+                  isIncomeTooLow ||
+                  (isValidatingEmptyField && !forms.monthlyIncome)
+                }
                 emitOnBlurInput={onBlurIncomeInput}
                 onFocus={onFocusIncomeField}
               />
@@ -1838,6 +1850,7 @@ export default function LoanCalculatorPage() {
                 handleChange={handleChange}
                 defaultValue={forms.age}
                 onShowDropdown={onShowDropdownAgeField}
+                isError={isValidatingEmptyField && !forms.age}
               />
               {isValidatingEmptyField && !forms.age
                 ? renderErrorMessageEmpty()
@@ -1863,17 +1876,8 @@ export default function LoanCalculatorPage() {
             <Button
               // not using "disabled" attrib because some func need to be run
               // when disabled button is clicked
-              version={
-                isDisableCtaCalculate
-                  ? ButtonVersion.Disable
-                  : ButtonVersion.PrimaryDarkBlue
-              }
+              version={ButtonVersion.PrimaryDarkBlue}
               secondaryClassName={styles.buttonSubmit}
-              disabled={
-                isDisableCtaCalculate ||
-                isLoadingCalculation ||
-                isLoadingInsuranceAndPromo
-              }
               size={ButtonSize.Big}
               onClick={onClickCalculate}
               data-testid={elementId.LoanCalculator.Button.HitungKemampuan}
