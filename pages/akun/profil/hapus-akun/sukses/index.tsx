fc6bd6c2955eacd5f61d6c60995e05f8c840779d
ErlanggaIfabula <erlangga.dwi@ifabula.com>
2023-10-26 11:36:57 +0700

    update profile

--- pages/akun/profil/hapus-akun/sukses/index.tsx
+++ pages/akun/profil/hapus-akun/sukses/index.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import styles from 'styles/pages/delete-account-success.module.scss'
-
-import { AxiosResponse } from 'axios'
 import clsx from 'clsx'
 import { useRouter } from 'next/router'
 import { getSessionStorage } from 'utils/handler/sessionStorage'
@@ -9,35 +7,77 @@ import { getToken } from 'utils/handler/auth'
 import { SessionStorageKey } from 'utils/enum'
 import { CityOtrOption } from 'utils/types'
 import { api } from 'services/api'
-import { AnnouncementBoxDataType } from 'utils/types/utils'
+import { MobileWebTopMenuType } from 'utils/types/utils'
 import { rootUrl } from 'utils/helpers/routes'
 import { HeaderMobile } from 'components/organisms'
 import { Button } from 'components/atoms'
 import { ButtonSize, ButtonVersion } from 'components/atoms/button'
-import { CitySelectorModal } from 'components/molecules'
 import Image from 'next/image'
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
+import { useAnnouncementBoxContext } from 'services/context/announcementBoxContext'
+import { useUtils } from 'services/context/utilsContext'
+import { MobileWebFooterMenuType } from 'utils/types/props'
+import { useAfterInteractive } from 'utils/hooks/useAfterInteractive'
+import dynamic from 'next/dynamic'
+
+const CitySelectorModal = dynamic(
+  () => import('components/molecules').then((mod) => mod.CitySelectorModal),
+  { ssr: false },
+)
 
 const MainImage = '/revamp/illustration/approved.webp'
 
-export default function index() {
+export default function SuccessHapusAkun({
+  dataMobileMenu,
+  dataFooter,
+  dataCities,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const router = useRouter()
 
   const [isActive, setIsActive] = useState(false)
   const [isOpenCitySelectorModal, setIsOpenCitySelectorModal] = useState(false)
-  const [showAnnouncementBox, setShowAnnouncementBox] = useState<
-    boolean | null
-  >(
-    getSessionStorage(
-      getToken()
-        ? SessionStorageKey.ShowWebAnnouncementLogin
-        : SessionStorageKey.ShowWebAnnouncementNonLogin,
-    ),
-  )
-  const [cityListApi, setCityListApi] = useState<Array<CityOtrOption>>([])
+  const {
+    cities,
+    dataAnnouncementBox,
+    saveDataAnnouncementBox,
+    saveMobileWebTopMenus,
+    saveMobileWebFooterMenus,
+    saveCities,
+  } = useUtils()
+  const { showAnnouncementBox, saveShowAnnouncementBox } =
+    useAnnouncementBoxContext()
   const [countdown, setCountdown] = useState(5)
 
   useEffect(() => {
-    checkCitiesData()
+    document.body.style.overflowY = isActive ? 'hidden' : 'auto'
+    return () => {
+      document.body.style.overflowY = 'auto'
+    }
+  }, [isActive])
+
+  const getAnnouncementBox = async () => {
+    try {
+      const res: any = await api.getAnnouncementBox({
+        headers: {
+          'is-login': getToken() ? 'true' : 'false',
+        },
+      })
+      saveDataAnnouncementBox(res.data)
+    } catch (error) {}
+  }
+
+  const navigateHandler = () => {
+    router.push(rootUrl)
+  }
+
+  const onClickCta = () => {
+    navigateHandler()
+  }
+
+  useEffect(() => {
+    saveMobileWebTopMenus(dataMobileMenu)
+    saveMobileWebFooterMenus(dataFooter)
+    saveCities(dataCities)
     getAnnouncementBox()
 
     const timer = setTimeout(() => {
@@ -54,42 +94,22 @@ export default function index() {
     }
   }, [])
 
-  useEffect(() => {
-    document.body.style.overflowY = isActive ? 'hidden' : 'auto'
-    return () => {
-      document.body.style.overflowY = 'auto'
+  useAfterInteractive(() => {
+    if (dataAnnouncementBox) {
+      const isShowAnnouncement = getSessionStorage(
+        getToken()
+          ? SessionStorageKey.ShowWebAnnouncementLogin
+          : SessionStorageKey.ShowWebAnnouncementNonLogin,
+      )
+      if (typeof isShowAnnouncement !== 'undefined') {
+        saveShowAnnouncementBox(isShowAnnouncement as boolean)
+      } else {
+        saveShowAnnouncementBox(true)
+      }
+    } else {
+      saveShowAnnouncementBox(false)
     }
-  }, [isActive])
-
-  const checkCitiesData = () => {
-    if (cityListApi.length === 0) {
-      api.getCities().then((res) => {
-        setCityListApi(res)
-      })
-    }
-  }
-
-  const getAnnouncementBox = () => {
-    api
-      .getAnnouncementBox({
-        headers: {
-          'is-login': getToken() ? 'true' : 'false',
-        },
-      })
-      .then((res: AxiosResponse<{ data: AnnouncementBoxDataType }>) => {
-        if (res.data === undefined) {
-          setShowAnnouncementBox(false)
-        }
-      })
-  }
-
-  const navigateHandler = () => {
-    router.push(rootUrl)
-  }
-
-  const onClickCta = () => {
-    navigateHandler()
-  }
+  }, [dataAnnouncementBox])
 
   return (
     <div className={styles.container}>
@@ -100,7 +120,7 @@ export default function index() {
           position: 'fixed',
         }}
         emitClickCityIcon={() => setIsOpenCitySelectorModal(true)}
-        setShowAnnouncementBox={setShowAnnouncementBox}
+        setShowAnnouncementBox={saveShowAnnouncementBox}
         isShowAnnouncementBox={showAnnouncementBox}
       />
       <div
@@ -149,8 +169,43 @@ export default function index() {
       <CitySelectorModal
         isOpen={isOpenCitySelectorModal}
         onClickCloseButton={() => setIsOpenCitySelectorModal(false)}
-        cityListFromApi={cityListApi}
+        cityListFromApi={cities}
       />
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps<{
+  dataMobileMenu: MobileWebTopMenuType[]
+  dataFooter: MobileWebFooterMenuType[]
+  dataCities: CityOtrOption[]
+}> = async (ctx) => {
+  ctx.res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=59, stale-while-revalidate=3000',
+  )
+
+  try {
+    const [menuMobileRes, footerRes, cityRes]: any = await Promise.all([
+      api.getMobileHeaderMenu(),
+      api.getMobileFooterMenu(),
+      api.getCities(),
+    ])
+
+    return {
+      props: {
+        dataMobileMenu: menuMobileRes.data,
+        dataFooter: footerRes.data,
+        dataCities: cityRes,
+      },
+    }
+  } catch (e) {
+    return {
+      props: {
+        dataMobileMenu: [],
+        dataFooter: [],
+        dataCities: [],
+      },
+    }
+  }
 }
