c7adecf692ae04fd3edfda7a0ae757c529829144
ridwanmaelfa <ridwan.maelfa@ifabula.com>
2023-11-01 14:47:19 +0700

    fix-changes

--- components/organisms/searchWidget/index.tsx
+++ components/organisms/searchWidget/index.tsx
@@ -611,7 +611,7 @@ const SearchWidget = () => {
           onClick={submit}
           data-testid={elementId.Homepage.Button.CariMobil}
         >
-          Cari Mobil
+          Cari Mobil Baru
         </Button>
       </div>
     </div>
